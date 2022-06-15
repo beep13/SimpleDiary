4b2eb0efcbd8e6185a13215cc39d80068b0549f9
beep13 <97983228+beep13@users.noreply.github.com>
2022-06-14 23:31:40 -0700

    sqlite connection

--- database.py
+++ database.py
@@ -1,9 +1,9 @@
-entries = []
+import sqlite3
+connection = sqlite3.connect("data.db")
 
 
 def add_entry(entry_content, entry_date):
     entries.append({"content": entry_content, "date": entry_date})
-
 
 
 def get_entries():
